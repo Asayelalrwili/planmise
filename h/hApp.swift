bc3b274d066ac5211199ab35210ff40a4c48d43b
asayel <example.com>
2023-01-10 19:28:01 +0300

    Initial Commit

---
+++ h/hApp.swift
@@ -0,0 +1,17 @@
+//
+//  hApp.swift
+//  h
+//
+//  Created by asayel on 17/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct hApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
